ce06493b0c936bf91227043cf77fc0bb1a0a4f72
Shihaix <109948660+Shihaix@users.noreply.github.com>
2026-04-16 01:26:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,24 +7,20 @@ SOURCE_URL = "https://raw.githubusercontent.com/Shihaix/Pluto-TV-Playlists/main/
 CHANNEL_ID = "6675c7868768aa0008d7f1c7"
 
 @app.route("/")
-def playlist():
+def stream_only():
     try:
         res = requests.get(SOURCE_URL, timeout=10)
         res.raise_for_status()
         lines = res.text.splitlines()
 
-        output = ["#EXTM3U"]
-
         for i in range(len(lines)):
             line = lines[i]
 
             if line.startswith("#EXTINF") and CHANNEL_ID in line:
-                output.append(line)
                 if i + 1 < len(lines):
-                    output.append(lines[i+1])
-                break  # only one channel
+                    return Response(lines[i+1], mimetype="text/plain")
 
-        return Response("\n".join(output), mimetype="text/plain")
+        return "Channel not found"
 
     except Exception as e:
         return f"Error: {str(e)}"
