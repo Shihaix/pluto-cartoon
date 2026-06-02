fbe424bda72a71b7a3c983fd927bc20b11b98a87
Shihaix <109948660+Shihaix@users.noreply.github.com>
2026-06-02 23:03:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
-@app.route("/Pokémon")
-def spotlight():
+from flask import Flask, Response
+import requests
+
+app = Flask(__name__)
+
+SOURCE_URL = "https://raw.githubusercontent.com/Shihaix/Pluto-TV-Playlists/main/output/plutotv_us.m3u8"
+
+@app.route("/pokemon")
+def pokemon():
     CHANNEL_ID = "6675c7868768aa0008d7f1c7"
 
     r = requests.get(SOURCE_URL, timeout=15)
